3a1944f325443deea60209777cc2ae3f88820132
Ariel Paricagua <ArielFParicaguaV@gmail.com>
2023-10-19 10:16:28 -0400

    useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// import { HooksApp } from './HooksApp'
+import { CounterApp } from './01-useState/CounterApp'
+
 import './index.css'
-import { HooksApp } from './HooksApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HooksApp />
+    <CounterApp />
   </React.StrictMode>,
 )
